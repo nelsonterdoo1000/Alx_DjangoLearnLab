f7078901bce6d4ab5acb53f4b4f760557b924e5d
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-07-13 08:43:47 +0100

    added custom admin functionality

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author','publication_year')
+    search_fields = ('title','author','publication_year')
+    list_filter = ('title',)
+admin.site.register(Book,BookAdmin)
