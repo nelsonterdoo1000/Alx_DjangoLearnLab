95c8edd9668060da677fd7ec1d0dec09f6901e49
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-07-21 09:28:55 +0100

    edited the code to meet checker requirement

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,7 +5,7 @@ book1 = Book.objects.get(id=1)
 
 
 #List all books in a library.
-library1 = Library.objects.get(name='Francis Idachaba')
+library1 = Library.objects.get(name='library_name')
 print(library1)
 
 #Retrieve the librarian for a library.
