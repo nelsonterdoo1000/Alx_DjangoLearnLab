7a0285572f7c6a596cedcd332a0fc0eb4567e1c5
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-07-27 21:02:16 +0100

    converted the class based views to function based views to meet checker requirement

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
-from relationship_app.views import RegisterView
 from django.contrib.auth.views import LoginView,LogoutView
+from . import views
 
 
 
@@ -12,5 +12,5 @@ urlpatterns = [
     path('library_details/<int:pk>/',LibraryDetailView.as_view()),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('signup/',RegisterView.as_view(template_name='relationship_app/register.html'),name='signup'),
+    path('signup/',views.register,name='signup'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,8 +6,11 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+
 
 def list_books(request):
 
@@ -22,7 +25,17 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-class RegisterView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'relationship_app/register.html'
+# class RegisterView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'relationship_app/register.html'
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse_lazy('login'))
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
