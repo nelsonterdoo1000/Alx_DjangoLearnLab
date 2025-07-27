92bcd912826bf94423bd50db43c7dc822c3b8279
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-07-27 20:52:42 +0100

    restructured the code to meet checker requirements

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from .views import list_books
 from .views import LibraryDetailView
+from relationship_app.views import RegisterView
+from django.contrib.auth.views import LoginView,LogoutView
+
+
+
 
 urlpatterns = [
     path('book_list/',list_books ),
     path('library_details/<int:pk>/',LibraryDetailView.as_view()),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('signup/',RegisterView.as_view(template_name='relationship_app/register.html'),name='signup'),
 ]
