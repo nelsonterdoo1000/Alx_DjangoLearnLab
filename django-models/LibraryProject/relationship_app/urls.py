fddb50a3991431b215fc5036607dfd72948263bc
nelsonterdoo1000 <nelsonterdoo1000@gmail.com>
2025-08-20 15:44:14 +0100

    changed the url patterns to meet checker requirement

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     path('admin_view/',admin_view, name='admin_view'),
     path('librarian_view/',librarian_view, name='librarian_view'),
     path('member_view/',member_view, name='member_view'),
-    path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/delete/', views.delete_book, name='delete_book'),
 
 ]
